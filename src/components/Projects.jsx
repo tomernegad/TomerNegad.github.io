71f5b3d8495780881abe568a9e8a8a4add0dec62
Tomer Negad <tomernegad@gmail.com>
2025-11-19 13:46:40 +0200

    Update project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,28 +6,28 @@ const Projects = () => {
       title: 'BookStore App',
       description: 'A full-featured bookstore application with user authentication and book management.',
       link: 'https://github.com/tomernegad/BookStore-App',
-      image: 'https://raw.githubusercontent.com/tomernegad/BookStore-App/main/frontend/images/bookslist.png',
+      image: 'https://private-user-images.githubusercontent.com/107677845/398362251-12654c93-9500-4aae-83cf-0f76cded1484.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjM1NTI5ODgsIm5iZiI6MTc2MzU1MjY4OCwicGF0aCI6Ii8xMDc2Nzc4NDUvMzk4MzYyMjUxLTEyNjU0YzkzLTk1MDAtNGFhZS04M2NmLTBmNzZjZGVkMTQ4NC5wbmciLCJuYmYiOjE3NjM1NTI2ODgsImV4cCI6MTc2MzU1MzU4OH0',
       tech: ['React', 'Node.js', 'MongoDB']
     },
     {
       title: 'FoodApp',
       description: 'A food delivery app with real-time ordering and payment integration.',
       link: 'https://github.com/tomernegad/FoodApp',
-      image: 'https://raw.githubusercontent.com/tomernegad/FoodApp/main/frontend/public/pizza.png',
+      image: 'https://private-user-images.githubusercontent.com/107677845/446948850-37a6de5c-6abb-4291-bd68-64fa62fc5426.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjM1NTI5ODgsIm5iZiI6MTc2MzU1MjY4OCwicGF0aCI6Ii8xMDc2Nzc4NDUvNDQ2OTQ4ODUwLTM3YTZkZTVjLTZhYmItNDI5MS1iZDY4LTY0ZmE2MmZjNTQyNi5wbmciLCJuYmYiOjE3NjM1NTI2ODgsImV4cCI6MTc2MzU1MzU4OH0',
       tech: ['React', 'Express', 'MongoDB']
     },
     {
       title: 'Todo App',
       description: 'A simple yet powerful todo application with drag-and-drop functionality.',
       link: 'https://github.com/tomernegad/todoapp',
-      image: 'https://raw.githubusercontent.com/tomernegad/todoapp/main/public/todo.png',
+      image: 'https://private-user-images.githubusercontent.com/107677845/416112564-3ae44b06-bc7c-4476-9515-d7a25d550490.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjM1NTI5ODgsIm5iZiI6MTc2MzU1MjY4OCwicGF0aCI6Ii8xMDc2Nzc4NDUvNDE2MTEyNTY0LTNhZTQ0YjA2LWJjN2MtNDQ3Ni05NTE1LWQ3YTI1ZDU1MDQ5MC5wbmciLCJuYmYiOjE3NjM1NTI2ODgsImV4cCI6MTc2MzU1MzU4OH0',
       tech: ['React', 'JavaScript']
     },
     {
       title: 'Booking App',
       description: 'A booking system for reservations with calendar integration.',
       link: 'https://github.com/tomernegad/BOOKINGAPP',
-      image: 'https://raw.githubusercontent.com/tomernegad/BOOKINGAPP/main/client/public/booking.png',
+      image: 'https://private-user-images.githubusercontent.com/107677845/409452179-afde8e1b-989e-4901-a46a-5053877035e0.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NjM1NTI5OTAsIm5iZiI6MTc2MzU1MjY5MCwicGF0aCI6Ii8xMDc2Nzc4NDUvNDA5NDUyMTc5LWFmZGU4ZTFiLTk4OWUtNDkwMS1hNDZhLTUwNTM4NzcwMzVlMC5wbmciLCJuYmYiOjE3NjM1NTI2OTAsImV4cCI6MTc2MzU1MzU5MH0',
       tech: ['React', 'Node.js', 'Express']
     }
   ];
